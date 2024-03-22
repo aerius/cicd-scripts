b186f5610bb2992ac45e4f85115ebdcb19104fca
Serhat Gülçiçek <serhat+github@equil.nl>
2024-03-22 20:02:00 +0100

    [job/notify_mattermost_message.sh] Improved handling of message title [job/get_build_name.sh] Uppercase all names because that looks cooler

--- job/get_build_name.sh
+++ job/get_build_name.sh
@@ -7,19 +7,19 @@ if [[ -n "${PULLREQUEST_ID}" ]]; then
   # Check whether the required PULLREQUEST_REPOSITORY is set
   : ${PULLREQUEST_REPOSITORY?'PULLREQUEST_REPOSITORY is required for this script to function'}
 
-  echo -n "${PULLREQUEST_REPOSITORY} PR#${PULLREQUEST_ID}"
+  echo -n "${PULLREQUEST_REPOSITORY^^} PR#${PULLREQUEST_ID}"
 elif [[ -z "${SOURCE_JOB_NAME}" ]]; then
   # Check whether the required vars are set
   : ${JOB_NAME?'JOB_NAME is required for this script to function'}
   : ${BUILD_NUMBER?'BUILD_NUMBER is required for this script to function'}
 
-  echo -n "${JOB_NAME} #${BUILD_NUMBER}"
+  echo -n "${JOB_NAME^^} #${BUILD_NUMBER}"
 else
   # Check whether the required vars are set
   : ${SOURCE_JOB_NAME?'SOURCE_JOB_NAME is required for this script to function'}
   : ${SOURCE_JOB_BUILD_NUMBER?'SOURCE_JOB_BUILD_NUMBER is required for this script to function'}
 
-  echo -n "${SOURCE_JOB_NAME} #${SOURCE_JOB_BUILD_NUMBER}"
+  echo -n "${SOURCE_JOB_NAME^^} #${SOURCE_JOB_BUILD_NUMBER}"
 fi
 
 # if terraform action executed is destroy add it to the build name
--- job/notify_mattermost_message.sh
+++ job/notify_mattermost_message.sh
@@ -7,12 +7,19 @@ function notify_mattermost_message_add_label() {
   echo '!['"${1}"'](https://nexus.aerius.nl/repository/resources/images/label_'"${1}"'.png)'
 }
 
-MSG_TITLE="[${BUILD_DISPLAY_NAME^^}](${BUILD_URL})"
+# Default title
+MSG_TITLE="[${JOB_NAME} ${BUILD_DISPLAY_NAME}](${BUILD_URL})"
+
+# If BUILD_DISPLAY_NAME contains a space, it's a custom one, use that instead
+[[ "${BUILD_DISPLAY_NAME}" == *' '* ]] && MSG_TITLE="[${BUILD_DISPLAY_NAME^^}](${BUILD_URL})"
+
 if [[ "${JOB_NAME}" == 'DEPLOY-OTA-ENVIRONMENT' ]]; then
   MSG_TITLE+=' '$(notify_mattermost_message_add_label 'deploy')
   if [[ -n "${DEPLOY_TERRAFORM_ACTION}" ]]; then
     MSG_TITLE+=' '$(notify_mattermost_message_add_label "${DEPLOY_TERRAFORM_ACTION}")
   fi
+else if [[ "${JOB_NAME}" == 'QA-'* ]]; then
+  :
 else
   MSG_TITLE+=' '$(notify_mattermost_message_add_label 'build')
 fi
