b167f9c1dd6adac8273d21f873caed7a485a8648
Serhat Gülçiçek <serhat+github@equil.nl>
2024-04-25 13:34:59 +0200

    [docker/get_image_tag.sh] Add (short) git commit hash to tags of PRs, or the current date if no commit information is present Also add date by default for other builds, so we can see when it was built.

--- docker/get_image_tag.sh
+++ docker/get_image_tag.sh
@@ -4,10 +4,12 @@
 set -e
 
 if [[ -n "${PULLREQUEST_ID}" ]]; then
-  echo -n "pr-${PULLREQUEST_ID}"
+  [[ -n "${GIT_COMMIT}" ]] && IMAGE_TAG_EXTRA=$(cut -c 1-8 <<< "${GIT_COMMIT}")
+  [[ -z "${IMAGE_TAG_EXTRA}" ]] && IMAGE_TAG_EXTRA=$(date +'%Y-%m-%d-%H-%M')
+  echo -n "pr-${PULLREQUEST_ID}_${IMAGE_TAG_EXTRA}"
 else
   # Check whether the required JOB_BASE_NAME is set
   : ${BUILD_NUMBER?'BUILD_NUMBER is required for this script to function'}
 
-  echo -n "build-${BUILD_NUMBER}"
+  echo -n "build-${BUILD_NUMBER}_"$(date +'%Y-%m-%d-%H-%M')
 fi
