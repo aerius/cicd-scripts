01c0b7385def37e1bda140edad65ea9a05265b32
Serhat Gülçiçek <serhat+github@equil.nl>
2025-11-12 17:10:27 +0100

    [prepare] Add support for ARM tooling At this time only needed for `yq`, as docker-compose is downloaded using docker which should pick the correct architecture automatically.

--- prepare/prepare-install-yq.sh
+++ prepare/prepare-install-yq.sh
@@ -5,7 +5,7 @@ set -e
 
 if ! [[ -x "${CICD_SCRIPTS_TOOLS_DIR}"/yq ]]; then
   echo '[prepare] # Installing yq'
-  curl --output "${CICD_SCRIPTS_TOOLS_DIR}"/yq -L https://github.com/mikefarah/yq/releases/download/"${CICD_SCRIPTS_YQ_VERSION}"/yq_linux_amd64
+  curl --output "${CICD_SCRIPTS_TOOLS_DIR}"/yq -L https://github.com/mikefarah/yq/releases/download/"${CICD_SCRIPTS_YQ_VERSION}"/yq_linux_"${CICD_SCRIPTS_TOOLS_ARCH}"
   chmod u+x "${CICD_SCRIPTS_TOOLS_DIR}"/yq
 else
   echo '[prepare] # yq is already installed, skipping install.'
