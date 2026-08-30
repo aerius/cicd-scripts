52bcd3ed6120a34f754941ca8827fe222a1f1f5f
Serhat Gülçiçek <serhat+github@equil.nl>
2026-08-30 13:07:36 +0200

    [cicdPipelineFlakyJob] Add some debugging info to help debug flaky instances

--- jenkins-library/vars/cicdPipelineFlakyJob.groovy
+++ jenkins-library/vars/cicdPipelineFlakyJob.groovy
@@ -15,6 +15,40 @@ def call(def build) {
       logLines.contains(hiccup)
     }
 
+    // If a hiccup is found, add some debugging info that might help find some solutions for the flakiness.
+    // These might fail if the disk is full, as Jenkins writes this to a temp file before executing.
+    if (foundHiccup) {
+      sh(script: '''#!/usr/bin/env sh
+            set +e
+            echo '### [cicdPipelineFlakyJob] - Hiccup detected, will print debugging information..'
+            echo
+
+            echo '### [cicdPipelineFlakyJob] - Disk usage'
+            df -Th
+            echo
+
+            echo '### [cicdPipelineFlakyJob] - Inode usage'
+            df -i
+            echo
+
+            echo '### [cicdPipelineFlakyJob] - Docker info'
+            docker info
+            echo
+
+            echo '### [cicdPipelineFlakyJob] - Docker Disk usage'
+            docker system df
+            echo
+
+            echo '### [cicdPipelineFlakyJob] - ECS Task Metadata'
+            curl -s --max-time 3 "${ECS_CONTAINER_METADATA_URI_V4}/task"
+            echo
+
+            # Exit cleanly, ignoring any errors
+            exit 0
+        '''
+      )
+    }
+
     // This will return null if nothing is found as well, otherwise will return what matches
     return foundHiccup
   }
